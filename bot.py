7244818324b41a2cc380a5639c53f7cb09a5e50d
Chaos <safinctg900@gmail.com>
2025-04-10 17:13:42 -0400

    more testing

--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the bot token from the .env file
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
@@ -11,8 +19,8 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send('Hello there!')
 
-bot.run('')
-
-
-
-#strawbears bot
+# Run the bot using the token from the .env file
+if BOT_TOKEN:
+    bot.run(BOT_TOKEN)
+else:
+    print("Error: BOT_TOKEN is not set in the .env file.")
