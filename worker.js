d7a747cefe1ad1f59b65a0f90c7a750dc5e7f6f5
Chaos <safinctg900@gmail.com>
2025-04-11 00:22:38 -0400

    clean up

--- worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    // Forward the request to the Python worker
-    const response = await env.PYTHON_WORKER.fetch(request);
-    return response;
-  },
-};
